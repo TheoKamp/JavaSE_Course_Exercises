544d17b32d78e1dca26fe1934cd0fe37380ad4c4
TheoKamp <77067397+TheoKamp@users.noreply.github.com>
2022-05-24 11:18:24 +0300

    Delete Lesson_9_Classes_And_Static_Members directory

--- Lesson_9_Classes_And_Static_Members/Exercise_01_Static_Memebers/staticMembers/src/Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-
-    public static void main(String[] args) {
-        Cycle cycle = new Cycle(1.0, "in");
-        System.out.println("Perimeter to cm: " + cycle.perimeterCm());
-        System.out.println("Perimeter to in: " + cycle.perimeterIn());
-    }
-}
