b686e783d43eb4adabeb926ed99f11ee29f250c4
TheoKamp <77067397+TheoKamp@users.noreply.github.com>
2022-05-24 10:57:14 +0300

    Add files via upload

---
+++ Lesson_1_Introduction_to_Java/exercise_01_many_commands.java
@@ -0,0 +1,18 @@
+/* Άσκηση 1: Πολλές εντολές
+    • Όπως είδαμε, στο πρόγραμμα hello.java, ορίζεται μια κλάση (η
+      Hello) η οποία περιέχει τη μέθοδο main, η οποία περιέχει μία
+      εντολή println
+    • Γενικά στη main μπορούμε να βάλουμε όσες εντολές θέλουμε
+      και ξέρουμε ότι θα εκτελεστούν σειριακά (η μία μετά την άλλη)
+    • Κατασκευάστε ένα νέο πρόγραμμα με όνομα exercise01 το
+      οποίο θα εκτυπώνει στην οθόνη το εξής:
+
+      Hello World!
+      I am going to learn Java!!
+*/
+public class exercise01_many_commands {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am going to learn Java!!");
+    }
+}
